34b6fb14f9310fc38500794c0ce0f8f92a869055
digit4 <dhaval.pantojee@gmail.com>
2025-07-05 19:06:23 +0530

    added snipe keymap

--- lua/plugins/snipe.lua
+++ lua/plugins/snipe.lua
@@ -1,4 +1,13 @@
 return {
   "leath-dub/snipe.nvim",
+  keys = {
+    {
+      "<leader><leader>",
+      function()
+        require("snipe").open_buffer_menu()
+      end,
+      desc = "Open Sniper buffer menu",
+    },
+  },
   opts = {},
 }
