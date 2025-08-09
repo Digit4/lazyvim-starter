73e36cb78b93d72f744509b58af24cccf37c0229
digit4 <dhaval.pantojee@gmail.com>
2025-08-09 16:24:31 +0530

    scope added for tab specific buffers
    keymaps to clear marks from file
    
    lazy-lock changes

---
+++ lua/plugins/scope.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "tiagovla/scope.nvim",
+    config = true,
+    enabled = true,
+  },
+  {
+    "akinsho/bufferline.nvim",
+    enabled = true,
+    opts = {
+      options = {
+        always_show_bufferline = true,
+      },
+    },
+  },
+}
