950f2153c4dbab6e0b561ec68f6861e4dfcdf05a
digit4 <dhaval.pantojee@gmail.com>
2026-02-20 18:57:53 +0530

    updates + macro changes + scratchpad md file

--- lua/config/macros.lua
+++ lua/config/macros.lua
@@ -1,6 +1,6 @@
 -- NOTE: CUSTOM MACROS
 local esc = vim.api.nvim_replace_termcodes("<ESC>", true, true, true)
 
-vim.fn.setreg("p", '"9yOconsole.log("digit4 ' .. esc .. '"9pa:' .. esc .. "ei, " .. esc .. '"9pA;' .. esc .. "")
+vim.fn.setreg("p", '"myOconsole.log("digit4 ' .. esc .. '"mpa:' .. esc .. "ei, " .. esc .. '"mpA;' .. esc .. "")
 vim.fn.setreg("f", "gsa)hi")
 -- NOTE: END
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,5 +2,8 @@ return {
   "snacks.nvim",
   opts = {
     scroll = { enabled = false },
+    scratch = {
+      ft = "markdown",
+    },
   },
 }
