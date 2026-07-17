b97f1001a084552ceb00759cd3d0308c3922e051
digit4 <dhaval.pantojee@gmail.com>
2026-07-17 12:04:24 +0530

    removed lazy lock

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -15,3 +15,21 @@ autocmd("TextYankPost", {
     vim.highlight.on_yank()
   end,
 })
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.gd",
+  callback = function()
+    vim.bo.filetype = "gdscript"
+  end,
+})
+
+-- -- Only trigger if we are in yadm mode (detected via env var)
+-- if os.getenv("YADM_MODE") then
+--   vim.api.nvim_create_autocmd("VimEnter", {
+--     callback = function()
+--       -- Force snacks explorer to show only git-tracked files if possible
+--       -- Or jump straight into the git-files picker
+--       require("snacks").picker.git_files()
+--     end,
+--   })
+-- end
