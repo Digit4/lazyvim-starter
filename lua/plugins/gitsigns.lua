2f2df19b776c9752cd10332e57f1e85af2a4637c
digit4 <dhaval.pantojee@gmail.com>
2026-07-17 12:07:15 +0530

    yadm related changes

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,6 +1,8 @@
 return {
   "lewis6991/gitsigns.nvim",
   opts = {
+    attach_to_untracked = false,
     current_line_blame = true,
+    base = os.getenv("GIT_DIR"),
   },
 }
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,9 +1,24 @@
+local YADM_MODE = os.getenv("GIT_WORK_TREE") == vim.fn.getcwd()
+print(YADM_MODE)
 return {
   "snacks.nvim",
   opts = {
     scroll = { enabled = false },
     scratch = {
       ft = "markdown",
+    },
+    explorer = {
+      replace_netrw = true,
+    },
+    picker = {
+      sources = {
+        explorer = {
+          git_status = true,
+          hidden = YADM_MODE,
+          ignored = YADM_MODE,
+          untracked = not YADM_MODE,
+        },
+      },
     },
   },
 }
